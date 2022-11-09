441657a3677b79a3b350ac57dd8d3d8a787f4a6a
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-11-09 19:42:29 +0630

    Implementing Mix Chart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Chart from "react-apexcharts";
 import { BarChart } from './components/BarChart';
+import { MixChart } from './components/MixChart';
 
 function App() {
   return (
     <div className="App">
           <BarChart/>
+          <MixChart/>
     </div>
   );
 }
